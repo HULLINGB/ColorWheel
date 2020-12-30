13b6bf714afaad121acd4dee55f521fea67a1b29
HULLINGB <bradley.hullinger@ucdenver.edu>
2020-12-30 08:54:52 -0700

    Create Triangle.py

---
+++ Triangle.py
@@ -0,0 +1,23 @@
+from turtle import *
+import time
+
+speed(1)
+bgcolor("black")
+
+
+penup()
+goto(-150, -100)
+fillcolor("yellow")
+begin_fill()
+pendown()
+
+forward(300)
+left(120)
+forward(300)
+left(120)
+forward(300)
+left(120)
+
+end_fill()
+time.sleep(10)
+hideturtle()
